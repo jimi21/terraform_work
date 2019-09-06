78935f64bf55ff3356eccdea90e80e951778c080
Jimi Patel <gopi21all18@gmail.com>
2019-09-06 11:21:35 +0530

    Update partition.sh

--- partition.sh
+++ partition.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
-fdisk /dev/sdc
-n
-p
-1
-2048
-104857599
-w
-EOF
-mkfs -t ext4 /dev/sdc1
-mkdir /datadrive
-mount /dev/sdc1 /datadrive
-echo /dev/sdc1  /datadrive ext4 defaults,nofail 0 2 >> /etc/fstab
+/sbin/mkfs -t ext4 /dev/sdc
+/bin/mkdir /datadrive
+/bin/mount /dev/sdc /datadrive
+/bin/echo /dev/sdc /datadrive ext4 defaults,nofail 0 0 >> /etc/fstab
